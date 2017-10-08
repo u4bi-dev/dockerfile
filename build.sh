b1677c395457447cf6479d8ede8b0c570f1afcc4
u4bi <rootcode10@gmail.com>
2017-10-08 16:20:43 +0900

    build shell script render

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+NAME='u4bi/ubuntu:16.04'
+DATA=(  'kor-lang'
+        'nodejs'
+        'rollup'
+        'angular-cli'
+        'ionic'
+     )
+
+echo '개발환경 조성을 위한 도커파일 빌드를 시작합니다'
+for ITEM in ${DATA[@]}
+do
+    docker build -t ${NAME} ${ITEM}
+done
+echo '모든 빌드가 완료되었습니다. 도커 이미지를 실행합니다.'
+./run.sh
