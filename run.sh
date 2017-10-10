3588034bd09deb77d1b8e1e8846b2ad9347a93b1
u4bi <rootcode10@gmail.com>
2017-10-11 01:23:00 +0900

    update clear shell

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+echo '컨테이너 명을 입력해주세요'
+read -r NAME
+echo '포워딩 포트를 입력해주세요'
+read -r FORWARD
 
-NAME='u4bi-dev'
 REPO='u4bi/ubuntu:16.04'
 
 HOST_DIR=~/Dev
 CONTAINER_DIR='/data'
-FORWARD=7777
 
 docker run -it --rm -p ${FORWARD}:${FORWARD} --name ${NAME} -v ${HOST_DIR}:${CONTAINER_DIR} ${REPO} /bin/bash
