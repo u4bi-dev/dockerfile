c85dc68d56914a3e51e14bdb6dd2f3e328acdb9f
u4bi <rootcode10@gmail.com>
2017-10-11 13:19:52 +0900

    update run shell script

--- run.sh
+++ run.sh
@@ -3,10 +3,18 @@ echo '컨테이너 명을 입력해주세요'
 read -r NAME
 echo '포워딩 포트를 입력해주세요'
 read -r FORWARD
+echo '인스턴스 컨테이너입니까? (y / n)'
+read -r INSTANCE
 
 REPO='u4bi/ubuntu:16.04'
 
 HOST_DIR=~/Dev
 CONTAINER_DIR='/data'
 
-docker run -it --rm -p ${FORWARD}:${FORWARD} --name ${NAME} -v ${HOST_DIR}:${CONTAINER_DIR} ${REPO} /bin/bash
+RM=''
+if [ ${INSTANCE} = "y" ];
+then
+    RM='--rm'
+fi
+
+docker run -it ${RM} -p ${FORWARD}:${FORWARD} --name ${NAME} -v ${HOST_DIR}:${CONTAINER_DIR} ${REPO} /bin/bash
