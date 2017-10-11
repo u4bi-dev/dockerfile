86bd2287073437bbeebff229f4a645b3c0bc8414
u4bi <rootcode10@gmail.com>
2017-10-11 14:01:10 +0900

    update git core dockerfile

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ echo '컨테이너 명을 입력해주세요'
 read -r NAME
 echo '포워딩 포트를 입력해주세요'
 read -r FORWARD
-echo '인스턴스 컨테이너입니까? (y / n)'
+echo '고정 컨테이너입니까? (y / n)'
 read -r INSTANCE
 
 REPO='u4bi/ubuntu:16.04'
@@ -12,7 +12,7 @@ HOST_DIR=~/Dev
 CONTAINER_DIR='/data'
 
 RM=''
-if [ ${INSTANCE} = "y" ];
+if [ "$INSTANCE" != "y" ];
 then
     RM='--rm'
 fi
