54bf0be56cf7842d12d4744f10ac06ac9c50e002
u4bi <rootcode10@gmail.com>
2017-10-14 02:17:18 +0900

    update vendor dockerfile

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ echo '포워딩 포트를 입력해주세요'
 read -r FORWARD
 echo '고정 컨테이너입니까? (y / n)'
 read -r INSTANCE
-
-REPO='u4bi/ubuntu:16.04'
+echo '지정하실 이미지가 있습니까?'
+read -r IMAGE
 
 HOST_DIR=~/Dev
 CONTAINER_DIR='/data'
@@ -17,4 +17,9 @@ then
     RM='--rm'
 fi
 
-docker run -it ${RM} -p ${FORWARD}:${FORWARD} --name ${NAME} -v ${HOST_DIR}:${CONTAINER_DIR} ${REPO} /bin/bash
+if [ "$IMAGE" == "" ];
+then
+    IMAGE='u4bi/ubuntu:16.04'
+fi
+
+docker run -it ${RM} -p ${FORWARD}:${FORWARD} --name ${NAME} -v ${HOST_DIR}:${CONTAINER_DIR} ${IMAGE} /bin/bash
